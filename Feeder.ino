84882aac672e8ea75d1063cff06420d40e6e5099
Ekaterina <108530800+piccolo-gatto@users.noreply.github.com>
2025-03-14 04:23:26 +0800

    Add responses and portion weight control

--- Feeder.ino
+++ Feeder.ino
@@ -7,57 +7,78 @@
 #include <WiFiUdp.h>
 #include <WiFi.h>
 #include <lib/GyverStepper.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include <WiFiClient.h>
+#include "HX711.h"   
 
+#define DT  23                                                
+#define SCK 22 
+#define SERVO_PIN 26
+#define SSID = "****";
+#define PASS = "********"; 
+
+
+HX711 scale;
 GStepper<STEPPER4WIRE> stepper(2048, 19, 18, 17, 16);
-#define SERVO_PIN 26 
 Servo servoMotor;  
-int pos = 0;
-String knownBLENames[] = {"OSHEYNIK_001", "OSHEYNIK_002"};
-int RSSI_THRESHOLD = -35;
-bool device_found;
-int scanTime = 2;
 BLEScan* pBLEScan;
-const char* ssid     = "111";
-const char* password = "11111111";
-
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP);
+
+float calibration_factor = 0.0014813;                         
+float units;                                                 
+float ounces;  
+
+int pos = 0;
+int RSSI_THRESHOLD = -40;
+int scanTime = 2;
+int size;
+int timezone;
+
+bool device_found;
+bool dir = true;
 
 String dayStamp;
 String timeStamp;
 
-
-int feedAmount = 10;
-bool dir = true;
-int timezone = 8;
-
-const byte feedTime[][2] = {
-  {6, 0},       
-  {12, 0},
-  {17, 0},
-  {22, 11},
-};
-
-
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+    HTTPClient http;
+    WiFiClient client;
     void onResult(BLEAdvertisedDevice advertisedDevice) {
 
     String deviceName = advertisedDevice.getName();
-
-      for (int i = 0; i < (sizeof(knownBLENames) / sizeof(knownBLENames[0])); i++)
-      {
-        if (deviceName == knownBLENames[i].c_str()) 
+    if (http.begin(client, "http://192.168.0.103:8000/collar/get_collars_by_feeder")){
+      http.addHeader("accept", "application/json");
+      http.addHeader("Content-Type", "application/json");
+      StaticJsonDocument<1> TempDataJSON;
+      TempDataJSON["feeder_name"] = "FEEDER_001";
+      String TempDataString;
+      serializeJson(TempDataJSON, TempDataString);
+      int httpResponseCode = http.POST(TempDataString);
+      String result = http.getString();
+      Serial.println(result);
+      if (httpResponseCode == 200){
+        StaticJsonDocument<200> data;
+        deserializeJson(data, result);
+        for(byte i = 0; i < sizeof(data); i++){
+          if (deviceName == data[i]) 
                         {
           device_found = true;
           
           Serial.println(advertisedDevice.toString().c_str());
                           break;
                         }
+        }
+      }
     }
+    http.end();
 }};
+
+
 void setup() {
   Serial.begin(115200);
-  BLEDevice::init("Feeder001");
+  BLEDevice::init("FEEDER_001");
   pBLEScan = BLEDevice::getScan();
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
   pBLEScan->setActiveScan(true);
@@ -66,7 +87,7 @@ void setup() {
   servoMotor.setPeriodHertz(50);
   servoMotor.attach(SERVO_PIN);
   servoMotor.write(0);
-  WiFi.begin(ssid, password);
+  WiFi.begin(sSID, PASS);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -79,17 +100,61 @@ void setup() {
   stepper.setSpeed(10000);
   stepper.setMaxSpeed(10000);
   stepper.setAcceleration(10000);
+  scale.begin(DT, SCK);                                      
+  scale.set_scale();                                         
+  scale.tare();                                              
+  scale.set_scale(calibration_factor); 
 }
 
 
 void loop() {
-  device_found = false;
+  byte feedTime[][2] = {{}};
+  HTTPClient http;
+  WiFiClient client;
   timeClient.forceUpdate();
-  for (byte i = 0; i < sizeof(feedTime) / 2; i++){  
-    if ((feedTime[i][0] == get_hour(timeClient.getHours().toInt(), timezone)) && (feedTime[i][1] == timeClient.getMinutes().toInt())){
+  if (http.begin(client, "http://192.168.0.103:8000/settings/get_settings_by_feeder")){
+    http.addHeader("accept", "application/json");
+    http.addHeader("Content-Type", "application/json");
+    StaticJsonDocument<1> TempDataJSON;
+    TempDataJSON["feeder_name"] = "FEEDER_001";
+    String TempDataString;
+    serializeJson(TempDataJSON, TempDataString);
+    int httpResponseCode = http.POST(TempDataString);
+    String result = http.getString();
+    Serial.println(result);
+    if (httpResponseCode == 200){
+      StaticJsonDocument<200> data;
+      deserializeJson(data, result);
+      size = data["size"];
+      timezone = data["timezone"];
+      for(byte i = 0; i < sizeof(data["schedule"]) / 2; i++){
+        for(byte j = 0; j < 2; j++){
+          feedTime[i][j] = data["schedule"][i][j];
+        }
+      }
+    }
+  }
+  http.end();
+  int hour = timeClient.getHours();
+  int minute = timeClient.getMinutes();
+  for (int i = 0; i < sizeof(feedTime) / 2; i++){  
+    if ((feedTime[i][0] == get_hour(hour, timezone)) && (feedTime[i][1] == minute)){
+      отправить вес остатка на сервер
+      while(check_size() < size){
         feed();
       }
-    } 
+      if (http.begin(client, "http://192.168.0.103:8000/activity/feeder_feed_activity")){
+        http.addHeader("accept", "application/json");
+        http.addHeader("Content-Type", "application/json");
+        StaticJsonDocument<1> TempDataJSON;
+        TempDataJSON["feeder"] = "FEEDER_001";
+        String TempDataString;
+        serializeJson(TempDataJSON, TempDataString);
+        int httpResponseCode = http.POST(TempDataString);
+      }
+    }
+  } 
+  device_found = false;
   BLEScanResults *foundDevices = pBLEScan->start(scanTime, false);
   for (int i = 0; i < foundDevices->getCount(); i++)
   {
@@ -99,30 +164,39 @@ void loop() {
       {
         Serial.println("its here");
         pos = 180;
+        break;
 
   }
-      
     else{
     pos = 0;
+    //отправить вес остатка на сервер
   }
   }
-  Serial.print("pos ");
+  Serial.print("pos = ");
   Serial.println(pos);
   servoMotor.write(pos);
   pBLEScan->clearResults();
+
+}
+
+int check_size(){
+  for (int i = 0; i < 10; i ++) {                             
+    units = + scale.get_units(), 10;                         
+  }
+  units = units / 10;                                         
+  ounces = units * 0.035274;                                 
+  Serial.print(int(ounces));                                     
+  Serial.println(" grams"); 
+  return int(ounces);
 }
 
 void feed() {
-
-  
-  int tmp_fa = feedAmount;
+  int tmp_fa = size;
   while (tmp_fa>0){
     delay(1);
     anti_jam();
     tmp_fa--;
   }
-
-  
 }
 
 void anti_jam() {
@@ -140,5 +214,4 @@ int get_hour(int hour, int timezone){
   else {
     return hour_in_timezone - 24;
   }
-
 }
